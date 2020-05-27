c9504cf06280e83e5f19dba1d42c65cca749bd58
johntanza <tanzajohn@gmail.com>
2020-05-27 09:44:40 -0400

    adds long to remove

--- cmd/remove.go
+++ cmd/remove.go
@@ -10,6 +10,8 @@ func init() {
 	var remove = &cobra.Command{
 		Use:   "remove",
 		Short: "Deletes urls from store",
+		Long: "The Delete command requires the exact url that was added" +
+			"to the index originally",
 		Run: func(cmd *cobra.Command, args []string) {
 			s := internal.NewSearch(indexPath)
 			if _, err := s.Delete(args); err != nil {
